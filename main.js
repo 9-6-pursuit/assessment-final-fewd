d76812e2a3af8d3cf4433d3c6c5a35fd75f399ad
salvagebar <salvagebar@gmail.com>
2023-04-04 19:46:00 -0400

    Logging movie list

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+  // Add code you want to run on page load here
+  fetch(`https://resource-ghibli-api.onrender.com/films`)
+    .then((response) => response.json())
+    .then((response) => {
+      populateFormDropdown(response);
+    })
+    .catch((err) => console.error(err));
 }
+
+let populateFormDropdown = (movies) => {
+  console.log(movies);
+};
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
