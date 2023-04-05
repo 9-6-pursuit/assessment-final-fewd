3805b5a419c4e0c3fe53ec241e30486991964b64
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-04-04 21:58:49 -0400

    First commit

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 
-
-
-
+fetch('https://resource-ghibli-api.onrender.com/films/')
+	.then(response => response.json())
+	.then(response => run(response))
+	.catch(err => console.error(err));
 
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
-function run() {
- // Add code you want to run on page load here
+function run(movies) {
+ // Add code you want to run on page load here\
+    let select = document.getElementById("titles");
+    movies.forEach(movie => {
+        let option = document.createElement("option");
+        option.value = movie.id;
+        option.textContent = movie.title;
+        select.append(option);
+    });
+}
+
+let selected = document.getElementById("titles").value;
+fetch('https://resource-ghibli-api.onrender.com/films/' + selected)
+	.then(response => response.json())
+	.then(response => showMovieInfo(response))
+	.catch(err => console.error(err));
+
+function showMovieInfo(movie) {
+    
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
