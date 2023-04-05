a776783384b1d27ad9ec23a4b145373ecbdf2645
Raymond <raymondjohnson@pursuit.org>
2023-04-04 21:58:00 -0400

    Work in progress!

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-
+let movieListing = []
+let movieSelector = document.getElementById('titles');
+let selectButton = document.getElementById('select')
+let movieDetails = document.getElementById('movie-details');
 
 
 
@@ -12,15 +15,35 @@ function run() {
 fetch(`https://resource-ghibli-api.onrender.com/films/`)
   .then(response => response.json())
   .then(response => {
-    Response = response
-    //populateFormDropdown(response)
-    console.log(response)
-  })
-  .catch(err => console.error(err));
+    createMovieDropDown(response)
+})
+.catch(err => console.error(err));
 
-  let episodeList = []
-
+  
 }
+
+const createMovieDropDown = (movies) => {
+  console.log(movies)
+  let counter = 0
+  for (const movie of movies) {
+      counter += 1
+      if (counter <= 20) {
+        movieListing.push(movie)
+          let newOption = document.createElement("option")
+          newOption.textContent = movie.title
+          newOption.value = movie.title
+          movieSelector.append(newOption)
+      }
+  }
+}
+
+selectButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('button clicked!')
+  let selectedMovie = movieSelector.value
+  
+})
+
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
