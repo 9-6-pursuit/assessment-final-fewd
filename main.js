b22a884156d166cbaac33af59ad5f92905376730
semajeidas <sadiejames@pursuit.org>
2023-04-04 21:58:01 -0400

    Added header photo and page text

--- main.js
+++ main.js
@@ -20,57 +20,57 @@ function run() {
     })
     .catch(err => console.error(err));
 
-    // let filmList = []
-    // let currentFilm = null;
+    let filmList = []
+    let currentFilm = null;
 
-    // let populateFormDropdown = films => {
-    //     let counter = 0
-    //     for (const film of films) {
-    //         counter += 1
-    //         if (counter <= 23) {
-    //             filmList.push(film)
-    //             let newOption = document.createElement("option")
-    //             newOption.textContent = films.title
-    //             newOption.value = films.title
-    //             filmSelector.append(newOption)
-    //         }
-    //     }
-    // }
+    let populateFormDropdown = films => {
+        let counter = 0
+        for (const film of films) {
+            counter += 1
+            if (counter <= 23) {
+                filmList.push(film)
+                let newOption = document.createElement("option")
+                newOption.textContent = films.title
+                newOption.value = films.title
+                filmSelector.append(newOption)
+            }
+        }
+    }
 
-    // submitButton.addEventListener("click", event => {
-    //     event.preventDefault()
+    submitButton.addEventListener("click", event => {
+        event.preventDefault()
 
 
-    //     detailContainer.innerHTML = ""
+        detailContainer.innerHTML = ""
 
-    //     let filmName = movieSelector.value
-    //     let film = findFilmByName(filmName)
-    //     currentFilm = film
+        let filmName = movieSelector.value
+        let film = findFilmByName(filmName)
+        currentFilm = film
 
-    //     let titleHeader = document.createElement("h3")
-    //     titleHeader.textContent = episode.name
-    //     detailContainer.append(titleHeader)
+        let titleHeader = document.createElement("h3")
+        titleHeader.textContent = episode.name
+        detailContainer.append(titleHeader)
 
-    //     let description = document.createElement("p")
-    //     description.textContent = episode.description
-    //     detailContainer.append(description)
-    // })
+        let description = document.createElement("p")
+        description.textContent = episode.description
+        detailContainer.append(description)
+    })
 
-    // let findFilmByName = name => {
-    //     for (const film of filmList) {
-    //         if (film.name === name) {
-    //             return film
-    //         }
-    //     }
-    // }
+    let findFilmByName = name => {
+        for (const film of filmList) {
+            if (film.name === name) {
+                return film
+            }
+        }
+    }
 
-    // commentButton.addEventListener("click", event => {
-    //     event.preventDefault()
-    //     let newComment = document.createElement("li")
-    //     newComment.innerHTML += `<strong>${currentFilm.name}</strong>` + " -- "
-    //     newComment.innerHTML += commentInput.value
-    //     commentList.append(newComment)
-    // })
+    commentButton.addEventListener("click", event => {
+        event.preventDefault()
+        let newComment = document.createElement("li")
+        newComment.innerHTML += `<strong>${currentFilm.name}</strong>` + " -- "
+        newComment.innerHTML += commentInput.value
+        commentList.append(newComment)
+    })
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
