2776577c68dc7e67813ce7c10f4191d9a33102a0
paoAracena <Paolaaracena1233@gmail.com>
2023-04-04 20:25:57 -0400

    Dropdown

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+let dropdown = document.querySelector('select');
+
+fetch(`https://resource-ghibli-api.onrender.com/films/`).then(response => response.json()).then (movies => {
+ for(let i=0; i < movies.length; i++) {
+    let option = document.createElement('option');
+    option.textContent = movies[i].title;
+    dropdown.append(option);
+    option.setAttribute('value', movies[i].id);
+    
+ }// for loop
+// data.forEach(movies => {
+//     let option = document.createElement('option');
+//         option.textContent = movies.title;
+//         dropdown.append(option);
+//         option.setAttribute('value', movies.id);
+// })
+})//end of fetch
+
+
+
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
