f64739208ef3beab42b69473677cdfa326b264f3
antunishdPursuit <dennysantunish@pursuit.org>
2023-04-05 19:44:31 -0400

    Added comments

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ let count = 0
 let countTwo = 0
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 function run() {
+    // Fecthing the studio Ghibil API
     fetch(`https://resource-ghibli-api.onrender.com/films`)
         .then((response) => response.json())
         .then((json) => {
@@ -29,9 +30,9 @@ function run() {
         .catch((error) => {
             console.log(error);
         })
-
+// the function that the above fetch is calling with the API as a parameter
     const title = (json) => {
-
+// the movie details that is being inputed into the H3,p, and p tags
         json.forEach(element => {
             studioGhibilMovies.push(element)
         let optionMovie = document.createElement("option")
@@ -39,7 +40,7 @@ function run() {
             optionMovie.textContent = element.title
         selectMovie.append(optionMovie)
         });
-
+// the dropdown menu bar which will trigger the above code 
         selectMovie.addEventListener("change", event => {
             count++
             studioGhibilMovies.forEach(ele => {
@@ -56,6 +57,7 @@ function run() {
             orderedListItem.innerHTML = ""
             countTwo = 0
         })
+        // Creating the reviews for the selected movie and which will not dissappear unless the reset button is pressed 
         movieReview.addEventListener("submit", event => {
             event.preventDefault()
             console.log(count)
@@ -71,17 +73,16 @@ function run() {
 
             event.target.review.value = ""
         })
+        // when the below button is pressed, all the reviews submitted will dissappear 
         resetReview.addEventListener("click", event => {
             reviewList.innerHTML = ""
         })
-        
+        // when the show people button is pressed then the all the people within the movie api will be inputed into the ordered list 
         orderedList.addEventListener("click", event => {
             countTwo++
-            console.log(countTwo)
             studioGhibilMovies.forEach(ele => {
+                // makes sure the code is not triggered more than once and will reset after the change event listener is triggered 
                 if(ele.title === selectMovie.value){
-                   console.log(ele.people)
-                //    console.log(ele.people.includes('/people/'))
                     if (countTwo <= 1){
                         if(ele.people.includes('/people/')){
                             let item = document.createElement("li")
@@ -89,6 +90,7 @@ function run() {
                             orderedListItem.append(item)
                            } else {
                             ele.people.forEach(ele => {
+                                // loops through the people array found within the movie api and fecth the api for each person. The names will then populate the ordered list
                                 fetch(`https://resource-ghibli-api.onrender.com${ele}`)
                                 .then((response) => response.json())
                                 .then((json) => {
