8a57bf2886a8b39582dfb2dca8f64e01547e711c
fintheehuman <finessepoole@pursuit.org>
2023-04-04 22:12:14 -0400

    yass fail forward qween.

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
+const url = "https://resource-ghibli-api.onrender.com/films"
+
+const review = document.querySelector("form")
+console.log(review)
+
+review.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
+const filmz = document.querySelector("#films")
+console.log(filmz)
+
+// const titleSelections = document.createAttribute
+const movieSelector = document.querySelector("#movie-selector")
 
 
-
-
-
-
-// To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
-
-function run() {
- // Add code you want to run on page load here
-}
-
-// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
-// So that testing can work as expected for now
-// A non-hacky solution is being researched
-
-setTimeout(run, 1000);
+fetch(url)
+.then((response) => response.json())
+.then((films) => {
+    // console.log(films)
+    for (const film of films) {
+        const newOption = document.createElement("option")
+        newOption.textContent = film.title
+        newOption.value = film.title
+        movieSelector.append(newOption)
+    }
+})
