a16c726fca8611504094496c756da1b7be06b88e
salvagebar <salvagebar@gmail.com>
2023-04-04 21:27:47 -0400

    Movie details displaying correctly

--- main.js
+++ main.js
@@ -10,8 +10,55 @@ function run() {
     .catch((err) => console.error(err));
 }
 
+const movieSelector = document.getElementById("movie-selector");
+const movieSubmitButton = document.getElementById("movie-submit-button");
+const detailContainer = document.getElementById("display-info");
+const commentButton = document.getElementById("comment-submit-button");
+const commentInput = document.getElementById("comment-input");
+const commentList = document.getElementById("comment-list");
+
+let movieList = [];
+let currentMovie = null;
+
 let populateFormDropdown = (movies) => {
-  console.log(movies);
+  for (const movie of movies) {
+    console.log(movie);
+    movieList.push(movie);
+    let newOption = document.createElement("option");
+    newOption.textContent = movie.title;
+    newOption.value = movie.title;
+    movieSelector.append(newOption);
+  }
+};
+
+movieSubmitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  detailContainer.innerHTML = "";
+
+  let movieName = movieSelector.value;
+  let movie = findMovieByName(movieName);
+  currentMovie = movie;
+
+  let titleHeader = document.createElement("h3");
+  titleHeader.textContent = movie.title;
+  detailContainer.append(titleHeader);
+
+  let releaseDate = document.createElement("p");
+  releaseDate.textContent = movie.release_date;
+  detailContainer.append(releaseDate);
+
+  let description = document.createElement("p");
+  description.textContent = movie.description;
+  detailContainer.append(description);
+});
+
+let findMovieByName = (name) => {
+  for (const movie of movieList) {
+    if (movie.title === name) {
+      return movie;
+    }
+  }
 };
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
