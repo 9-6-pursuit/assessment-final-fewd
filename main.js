07d5ec6585756501e9eec7a5cbd0c1a263e614c4
Jerry John <jerryjohn@jerrys-air.myfiosgateway.com>
2023-04-04 21:45:44 -0400

    Not THE Final

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-
-
-
-
-
-
+const options = document.getElementById("titles")
+const films = []
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+    fetch('https://resource-ghibli-api.onrender.com/films')
+    .then((response) => response.json())
+    .then((films) => {
+      bringMeAMovie(films)
+    })
+    let bringMeAMovie = films => {
+    for (const movie of films) {
+        let newOption = document.createElement("option")
+       console.log(movie)
+        newOption.textContent = movie.title
+        newOption.value = movie.id
+        options.append(newOption)
+        }
+    }
 }
-
-// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
-// So that testing can work as expected for now
-// A non-hacky solution is being researched
-
-setTimeout(run, 1000);
