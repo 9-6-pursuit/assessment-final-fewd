aaba275376dab8828bdc454cca3e159195e5f186
fintheehuman <finessepoole@pursuit.org>
2023-04-13 00:09:48 -0400

    completed-ish assessment

--- main.js
+++ main.js
@@ -1,27 +1,97 @@
 const url = "https://resource-ghibli-api.onrender.com/films"
 
-const review = document.querySelector("form")
-console.log(review)
+const selector = document.querySelector(".movie-selector")
+// console.log(selector)
 
+const review = document.querySelector("form")
 review.addEventListener("submit", (event) => {
     event.preventDefault()
 })
-
-const filmz = document.querySelector("#films")
-console.log(filmz)
-
-// const titleSelections = document.createAttribute
-const movieSelector = document.querySelector("#movie-selector")
 
 
 fetch(url)
 .then((response) => response.json())
 .then((films) => {
-    // console.log(films)
+    console.log(films)
     for (const film of films) {
-        const newOption = document.createElement("option")
-        newOption.textContent = film.title
-        newOption.value = film.title
-        movieSelector.append(newOption)
+        const options = document.createElement("option")
+        options.textContent = film.title
+        options.value = film.title
+        selector.append(options)
+
     }
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const url = "https://resource-ghibli-api.onrender.com/films"
+
+// const review = document.querySelector("form")
+// console.log(review)
+
+// review.addEventListener("submit", (event) => {
+//     event.preventDefault()
+// })
+
+// const filmz = document.querySelector("#films")
+// console.log(filmz)
+
+// // const titleSelections = document.createAttribute
+// const movieSelector = document.querySelector("#movie-selector")
+
+
+// fetch(url)
+// .then((response) => response.json())
+// .then((films) => {
+//     // console.log(films)
+//     for (const film of films) {
+//         const newOption = document.createElement("option")
+//         newOption.textContent = film.title
+//         newOption.value = film.title
+//         movieSelector.append(newOption)
+//     }
+// })
